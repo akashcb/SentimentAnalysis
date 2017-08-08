fd0cdbfa1aaf3581a9335f3a547dd1a93e056752
Akash Bhatia <akash.akkiie@gmail.com>
2017-08-08 12:35:25 -0400

    code

---
+++ src/com/neu/MapClass.java
@@ -0,0 +1,80 @@
+package com.neu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.filecache.DistributedCache;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+
+public class MapClass extends Mapper<Object, Text, Text, IntWritable>{
+
+	private URI[] files;
+	private HashMap<String, String> AFINN_map = new HashMap<String, String>();
+
+	
+	public void setup(Context context) throws IOException
+	{
+		files = DistributedCache.getCacheFiles(context.getConfiguration());
+		System.out.println("files:"+ files);
+		Path path = new Path(files[0]);
+		FileSystem fs = FileSystem.get(context.getConfiguration());
+		FSDataInputStream in = fs.open(path);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String line="";
+
+		while((line = br.readLine()) != null)
+		{
+			String splits[] = line.split("\t");
+			AFINN_map.put(splits[0], splits[1]);
+		}
+		br.close();
+		in.close();
+	}
+	
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		
+		JSONParser jp = new JSONParser();
+		try {
+			
+			JSONObject obj = (JSONObject) jp.parse(value.toString());
+			if(obj.containsKey("asin") && obj.containsKey("reviewText"))
+			{
+				String review = obj.get("reviewText").toString();
+				System.out.println("Map:Serving: "+ review);
+				int sentiment_sum = 0;
+				String[] words = review.split(" ");
+				for(String word : words)
+				{
+					if(word.length() > 1 && AFINN_map.containsKey(word))
+					{
+						Integer s = new Integer(AFINN_map.get(word));
+						sentiment_sum += s;
+					}
+				}
+				String aboutProduct = obj.get("asin").toString();
+				context.write(new Text(aboutProduct), new IntWritable(sentiment_sum));
+			}
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+}
